85f70b41cbd9e93f1e0e6e3c2a58c98d6f69b123
Yasin Walum <ywalum@gmail.com>
2025-03-21 05:42:09 +0300

    change next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    // unoptimized: true,
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
